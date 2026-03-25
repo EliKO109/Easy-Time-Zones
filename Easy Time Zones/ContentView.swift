43c0c1793ff6f401685d36d4851cd228292a7a8c
EliKO109 <kony.design@gmail.com>
2026-03-25 13:44:45 -0500

    Initial Commit

---
+++ Easy Time Zones/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Easy Time Zones
+//
+//  Created by Eli Kony on 3/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Easy Time Zones/Easy_Time_ZonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Easy_Time_ZonesApp.swift
+//  Easy Time Zones
+//
+//  Created by Eli Kony on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Easy_Time_ZonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
